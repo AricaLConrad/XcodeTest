cf6eb51817e96516f4508069fe5b3ef79d0a23bf
AricaLConrad <aricaconrad@outlook.com>
2021-04-17 14:42:40 -0700

    Initial Commit

---
+++ Practice/PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeApp.swift
+//  Practice
+//
+//  Created by Student Account on 4/17/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
